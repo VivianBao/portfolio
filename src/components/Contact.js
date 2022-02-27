0a9fbbda2dcd83a0dc76e1f83df27bc02c1397b6
Vivian Bow <vivianbao1009@gmail.com>
2022-02-28 02:16:29 +0900

    Created title and form section inside form container

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,32 @@ import React from "react"
 import "../styles/Contact.scss"
 
 export default function Contact() {
+  const colorList = [["$gray", '103'], ["$light-blue", '152'], ["$blue", '117'], ["dark-blue", '84']]
+  const blockElements = colorList.map((set, index)=> {
+    return(
+      <div className={`block block-${index + 1}`} style={{backgroundColor: `${set[0]}`, height: `${set[1]}`}}></div>
+    )
+  })
   return(
     <div className="contact-container">
       <div className="form-container">
-        <h2>Contact</h2>
-        <form>
-          <label htmlFor="email">Email</label>
-          <input type="text" placeholder="Email" name="email" />
-          <label htmlFor="message">Message</label>
-          <textarea placeholder="Message" name="message"/>
-          <button>Submit</button>
-        </form>
+        <div className="title">
+          <h2>Contact</h2>
+          <div className="blocks">
+            {blockElements}
+          </div>
+        </div>
+        <div className="form">
+          <form>
+            <label htmlFor="email">Name</label>
+            <input type="text" placeholder="Name" name="Name" />
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="Email" name="email" />
+            <label htmlFor="message">Message</label>
+            <textarea placeholder="Message" name="message"/>
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   )
