349f4e152407928c072cc2d3113fe109793dd34e
Vivian Bow <vivianbao1009@gmail.com>
2022-02-28 01:52:55 +0900

    Created form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,19 @@
 import React from "react"
+import "../styles/Contact.scss"
 
 export default function Contact() {
   return(
-    <div>
-      <h1>This is contact</h1>
+    <div className="contact-container">
+      <div className="form-container">
+        <h2>Contact</h2>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="text" placeholder="Email" name="email" />
+          <label htmlFor="message">Message</label>
+          <textarea placeholder="Message" name="message"/>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
