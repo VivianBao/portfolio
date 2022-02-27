b8564b16f924df3f8992756e0b4d304442d0d718
Vivian Bow <vivianbao1009@gmail.com>
2022-02-28 00:06:43 +0900

    Created blocks and text, added triangle img

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,14 @@ export default function About() {
     <div className="about-container">
       <div className="about--brown-block">
         <div className="white-block">
-
+          <img className="triangle" src="/triangle.png" alt="triangle" />
+          <h2>About</h2>
+          <h5>My name is Wei An, and I'm a full stack web developer who's passaionate at analzing problems and find the solutions.<br/><br/>
+            I'm born and raised in Taiwan, and now living in Tokyo, Japan.<br/><br/>
+            I focused my studies on languages and cultures when I was in Waseda University, and then continued discovoring programming languages after graduation and found myself enchanted.<br/><br/>
+            I proceeded my journey by joining the full-time coding programme at Le Wagon, Tokyo.<br/><br/>
+            While being capable of working alone on projects, I particularly enjoy working in a team, in which you're given the opportunity to look at things from different perspectives and learn from other people.
+          </h5>
         </div>
       </div>
     </div>
