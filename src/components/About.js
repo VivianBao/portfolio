5c47215ed1efa59903c4147020774edb0bddba13
Vivian Bow <vivianbao1009@gmail.com>
2022-02-27 22:16:42 +0900

    Fixed navbar to overlay sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,14 @@
 import React from "react"
+import '../styles/About.scss'
 
 export default function About() {
   return (
     <div className="about-container">
+      <div className="about--brown-block">
+        <div className="white-block">
 
+        </div>
+      </div>
     </div>
   )
 }
