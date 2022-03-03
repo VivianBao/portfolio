cf44db2c7bbaa55fede479aefc21500a47b94fb0
Vivian Bow <vivianbao1009@gmail.com>
2022-03-03 17:15:01 +0900

    Created data.js

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const featuredProjects = [
+  {
+    id: 1,
+    title: 'Kikoemas+',
+    img: '',
+    tags: ["ruby on rails", "PostgreSQL", "html", "css", "javascript", "sass", "stimulus.js", "bootstrap", "figma"]
+  },
+  {
+    id: 2,
+    title: 'BARTER',
+    img: '',
+    tags: ["ruby on rails", "PostgreSQL", "html", "css", "javascript", "sass", "stimulus.js", "bootstrap", "figma"]
+  },
+  {
+    id: 3,
+    title: 'Animu',
+    img: '',
+    tags: ["ruby on rails", "PostgreSQL", "html", "css", "javascript", "sass", "stimulus.js", "bootstrap", "figma"],
+  },
+  {
+    id: 4,
+    title: 'Quizzical',
+    img: '',
+    tags: ["react.js", "html", "css", "javascript", "sass", "figma"]
+  }
+];
+
+export const railsProjects = [
+  {
+    id: 1,
+    title: 'Kikoemas+',
+    img: '',
+    tags: ["ruby on rails", "PostgreSQL", "html", "css", "javascript", "sass", "bootstrap", "figma"]
+  },
+  {
+    id: 2,
+    title: 'BARTER',
+    img: '',
+    tags: ["ruby on rails", "PostgreSQL", "html", "css", "javascript", "sass", "bootstrap", "figma"]
+  },
+  {
+    id: 3,
+    title: 'Animu',
+    img: '',
+    tags: ["ruby on rails", "PostgreSQL", "html", "css", "javascript", "sass", "bootstrap", "figma"],
+  },
+  {
+    id: 5,
+    title: 'Task Manager',
+    img: '',
+    tags: ["ruby on rails", "html", "css", "javascript", "sass", "bootstrap", "figma"],
+  }
+];
+
+export const reactProjects = [
+  {
+    id: 4,
+    title: 'Quizzical',
+    img: '',
+    tags: ["react.js", "html", "css", "javascript", "sass", "figma"]
+  },
+  {
+    id: 6,
+    title: 'Tenzies',
+    img: '',
+    tags: ["react.js", "html", "css", "javascript", "sass", "figma"]
+  },
+];
