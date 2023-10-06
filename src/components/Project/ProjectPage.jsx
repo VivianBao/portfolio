ff574f3703b96fc1056d08334d010be49bfc4d83
Vivian Bow <vivianbao1009@gmail.com>
2023-10-06 22:17:24 +0800

    Change product page structure, add responsive styling to product page

--- src/components/Project/ProjectPage.jsx
+++ src/components/Project/ProjectPage.jsx
@@ -36,7 +36,7 @@ export default function ProjectPage() {
 
   return(
     <>
-    <div className="project-landing-container">
+    {/* <div className="project-landing-container">
       <div className="left-container">
         <div className="left-landing-container">
           <div className="preview-container">
@@ -91,7 +91,69 @@ export default function ProjectPage() {
           <ImageGallery
             items={images}
             showBullets={true}
-            // showIndex={true}
+            showThumbnails={false}
+            lazyLoad={true}
+            showPlayButton={false}
+            showFullscreenButton={false}
+           />
+      )}
+      </div>
+    </div> */}
+    <div className="project-landing-container">
+      <div className="top-container">
+        <div className="left-landing-container">
+          <div className="preview-container">
+            <div className={`${projectData.img[0].type === "desktop" ? "photo-desktop" : "photo-mobile"}`} style={{
+                  backgroundImage: projectData.img[0].url
+                }}></div>
+          </div>
+          <div className="title-container">
+            <h2>{projectData.title}</h2>
+            <h3>{projectData.sub_title}</h3>
+          </div>
+          <div className="icons">
+            {iconElements}
+          </div>
+        </div>
+        <div className="introduction-container">
+          <div className="introduction-title">
+            <h2>Introduction</h2>
+            <a href={projectData.code} alt="code" target="_blank">View Code</a>
+          </div>
+          <div className="content-container">
+            <h5>{projectData.story}</h5>
+          </div>
+          {projectData.team ? (
+            <>
+              <h3>Team Members</h3>
+              <div className="team-container">
+                {projectData.team.map(member => {
+                  return(
+                    <div>
+                      <h5>{member}</h5>
+                    </div>
+                  )
+                  })}
+              </div>
+            </>
+
+          ):(<></>)}
+        </div>
+      </div>
+      <div className="bottom-container">
+        <div className="features-container">
+          <h2>Features</h2>
+          {projectContent}
+        </div>
+      {projectData.title === "Kikoemas+" ? (
+        <div className="video-container" >
+          <h3>Product Pitch on Demo Day</h3>
+          <iframe width="672" height="378" src="https://www.youtube.com/embed/OP88DtltO2Y?si=bBWxfzcUB036Vke1&amp;start=953&end=1392" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+      ):(
+          <ImageGallery
+            items={images}
+            showBullets={true}
             showThumbnails={false}
             lazyLoad={true}
             showPlayButton={false}
